b161ac7b664c7c4f2cd37aa7491c440baafdbf07
David Adafia <davidadafia@gmail.com>
2017-07-24 20:54:23 +0100

    map

--- app/assets/javascripts/grayscale.js
+++ app/assets/javascripts/grayscale.js
@@ -42,7 +42,7 @@ google.maps.event.addDomListener(window, 'resize', function() {
 
 function init() {
     // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+    
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
         zoom: 15,
