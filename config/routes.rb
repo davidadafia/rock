60c76f8b587c3a70ee26b827f1452dd691fc8840
David Adafia <davidadafia@gmail.com>
2017-10-09 12:43:37 +0100

    as

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'services/index'
 
+  get 'contact/index'
+
   get 'homes/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
